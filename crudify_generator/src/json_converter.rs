ab284bdcdfedd53952993f534c0f7305d43a1b77
Sven Richter <sveri80@gmail.com>
2022-10-02 10:23:16 +0200

    added datatype parser for property object

--- crudify_generator/src/json_converter.rs
+++ crudify_generator/src/json_converter.rs
@@ -90,14 +90,13 @@ fn parse_properties(value: &Value) -> Result<IndexMap<String, String>, JsonConve
             if property_key == "id" && property_value.is_object() {
                 let property_type = as_object_with_context(property_value, value)?;
                 let mut data_type = "String";
-                // if property_type.contains_key("format") {
-                //     data_type = DATATYPE_FORMAT_TO_RUST_DATATYPE.get(property_type.get("format").unwrap().as_str().unwrap()).unwrap();
-                // }
                 if let Some(format) = property_type.get("format") {
                     if DATATYPE_FORMAT_TO_RUST_DATATYPE.contains_key(format.as_str().unwrap()) {
                         data_type = DATATYPE_FORMAT_TO_RUST_DATATYPE.get(format.as_str().unwrap()).unwrap();
+                    } else if let Some(type_value) = property_type.get("type") {
+                        data_type = DATATYPE_TO_RUST_DATATYPE.get(type_value.as_str().unwrap()).unwrap();
                     } else {
-                        data_type = DATATYPE_TO_RUST_DATATYPE.get(property_type.get("type").unwrap().as_str().unwrap()).unwrap();
+                        warn!("No type or format found in data type property, assuming string as default. Value: {:?}", value);
                     }
                 }
 
@@ -138,21 +137,29 @@ mod tests {
         assert_eq!("i64".to_string(), models.get(0).unwrap().properties.as_ref().unwrap().get("id").unwrap().to_string());
     }
 
-    // #[test]
-    // fn with_wrong_id_property() {
-    //     init();
-    //     let order_with_id = json!({"Order": {"type": "object", "properties": {"id": "foobar"}}});
-    //     let models = convert_to_internal_model(&order_with_id);
-    //     assert_eq!("Order", models.get(0).unwrap().name);
-    // }
+    #[test]
+    fn with_id_property_without_type_object() {
+        let order_with_id = json!({"Order": {"type": "object", "properties": {"id": {}}}});
+        let models = convert_to_internal_model(&order_with_id).unwrap();
+        assert_eq!("Order", models.get(0).unwrap().name);
+        assert_eq!("String".to_string(), models.get(0).unwrap().properties.as_ref().unwrap().get("id").unwrap().to_string());
+    }
 
-    // #[test]
-    // fn without_properties() {
-    //     let two_order_objects = json!({"Order": {}, "OrderTwo": {}});
-    //     let models = convert_to_internal_model(&two_order_objects);
-    //     assert_eq!("Order", models.get(0).unwrap().name);
-    //     assert_eq!("OrderTwo", models.get(1).unwrap().name);
-    // }
+    #[test]
+    fn with_wrong_id_property() {
+        init();
+        let order_with_id = json!({"Order": {"type": "object", "properties": {"id": "foobar"}}});
+        let models = convert_to_internal_model(&order_with_id).unwrap();
+        assert_eq!("Order", models.get(0).unwrap().name);
+    }
+
+    #[test]
+    fn without_properties() {
+        let two_order_objects = json!({"Order": {}, "OrderTwo": {}});
+        let models = convert_to_internal_model(&two_order_objects).unwrap();
+        assert_eq!("Order", models.get(0).unwrap().name);
+        assert_eq!("OrderTwo", models.get(1).unwrap().name);
+    }
 
     fn init() {
         let _ = env_logger::builder().is_test(true).filter_level(log::LevelFilter::Debug).try_init();
