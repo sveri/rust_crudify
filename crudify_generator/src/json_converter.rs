c26eec10f0ac3811c74c696eb536269303a631f2
Sven Richter <sveri80@gmail.com>
2022-10-02 09:57:25 +0200

    added static datatype map

--- crudify_generator/src/json_converter.rs
+++ crudify_generator/src/json_converter.rs
@@ -3,31 +3,35 @@
 use std::{error::Error, fmt::Display};
 
 use indexmap::IndexMap;
-use log::{warn, info, error};
+use log::{error, info, warn};
 use phf::phf_map;
-use serde_json::{Value};
+use serde_json::{Map, Value};
 
-use crate::{InternalModels, InternalModel};
+use crate::{InternalModel, InternalModels};
 
 // from https://www.reddit.com/r/rust/comments/gj8inf/comment/fqlmknt/
 #[derive(Debug)]
-pub enum JsonConverterError {
-    AsObjectError,
+pub enum JsonConverterError<'a> {
+    // AsObjectError,
+    AsObjectError { value: &'a Value },
 }
 
-impl Error for JsonConverterError {
+impl Error for JsonConverterError<'_> {
+    // fn source(&self) -> Option<&(dyn Error + 'static)> {
+
+    // }
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match *self {
-            Self::AsObjectError => None
+            JsonConverterError::AsObjectError { ref value } => None,
         }
     }
 }
 
-impl Display for JsonConverterError {
+impl Display for JsonConverterError<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match *self {
-            Self::AsObjectError => {
-                write!(f, "Could not convert value to object")
+            Self::AsObjectError { ref value } => {
+                write!(f, "Could not convert value to object: {}", value)
             }
         }
     }
@@ -54,73 +58,85 @@ static DATATYPE_TO_RUST_DATATYPE: phf::Map<&'static str, &'static str> = phf_map
     "number" => "f64"
 };
 
-
 pub fn convert_to_internal_model(j: &Value) -> Result<InternalModels, JsonConverterError> {
     let mut internal_models = Vec::new();
-    for(key, value) in j.as_object().unwrap() {
+    for (key, value) in j.as_object().unwrap() {
         if value.is_object() {
             let properties = parse_properties(&value)?;
-    
-            internal_models.push(InternalModel{name: key.to_string(), properties: Some(properties)})
+
+            internal_models.push(InternalModel {
+                name: key.to_string(),
+                properties: Some(properties),
+            })
         }
     }
 
     Ok(internal_models)
 }
 
+fn as_object(value: &Value) -> Result<&Map<String, Value>, JsonConverterError> {
+    value.as_object().ok_or(JsonConverterError::AsObjectError { value: value })
+}
+
+fn as_object_with_context<'a>(value: &'a Value, ctx_value: &'a Value) -> Result<&Map<String, Value>, JsonConverterError> {
+    value.as_object().ok_or(JsonConverterError::AsObjectError { value: ctx_value })
+}
 
 fn parse_properties(value: &Value) -> Result<IndexMap<String, String>, JsonConverterError> {
     let mut property_map: IndexMap<String, String> = IndexMap::new();
-    // let o = value.as_object().ok_or(format!("parameter value is not an object: {:?}", value))?;
-    let o = value.as_object().ok_or(JsonConverterError::AsObjectError)?;
+    let o = as_object(value)?;
     if let Some(properties) = o.get("properties") {
-
-        for(property_key, property_value) in properties.as_object().ok_or(JsonConverterError::AsObjectError)? {
+        for (property_key, property_value) in as_object_with_context(properties, value)? {
             if property_key == "id" && property_value.is_object() {
-                
-                let property_type = property_value.as_object().unwrap();
+                let property_type = as_object_with_context(property_value, value)?;
                 let mut data_type = "String";
-                if property_type.contains_key("format") {
-                    data_type = DATATYPE_FORMAT_TO_RUST_DATATYPE.get(property_type.get("format").unwrap().as_str().unwrap()).unwrap();
+                // if property_type.contains_key("format") {
+                //     data_type = DATATYPE_FORMAT_TO_RUST_DATATYPE.get(property_type.get("format").unwrap().as_str().unwrap()).unwrap();
+                // }
+                if let Some(format) = property_type.get("format") {
+                    if DATATYPE_FORMAT_TO_RUST_DATATYPE.contains_key(format.as_str().unwrap()) {
+                        data_type = DATATYPE_FORMAT_TO_RUST_DATATYPE.get(format.as_str().unwrap()).unwrap();
+                    } else {
+                        data_type = DATATYPE_TO_RUST_DATATYPE.get(property_type.get("type").unwrap().as_str().unwrap()).unwrap();
+                    }
                 }
 
                 property_map.insert(property_key.to_string(), data_type.to_string());
             } else {
                 warn!("The id value must be an object, but was {:?}", property_value);
             }
-
         }
     }
 
     Ok(property_map)
 }
 
-
-
 #[cfg(test)]
 mod tests {
     use super::*;
     use serde_json::json;
-
 
     #[test]
     fn non_object_value_must_err() {
         let order_with_id = json!({"Order": {"type": "object", "properties": []}});
         let models = convert_to_internal_model(&order_with_id);
         assert!(models.is_err());
-        let err = models.unwrap_err();
-        assert_eq!("sdlkfj", err.to_string());
-        // assert_eq!("Order", models.unwrap().get(0).unwrap().name);
-        // assert_eq!("i64".to_string(), models.get(0).unwrap().properties.as_ref().unwrap().get("id").unwrap().to_string());
+        assert_eq!(
+            JsonConverterError::AsObjectError {
+                value: &json!({"type": "object", "properties": []})
+            }
+            .to_string(),
+            models.unwrap_err().to_string()
+        );
     }
 
-    // #[test]
-    // fn with_id_property() {
-    //     let order_with_id = json!({"Order": {"type": "object", "properties": {"id": {"type": "integer", "format": "int64"}}}});
-    //     let models = convert_to_internal_model(&order_with_id);
-    //     assert_eq!("Order", models.get(0).unwrap().name);
-    //     assert_eq!("i64".to_string(), models.get(0).unwrap().properties.as_ref().unwrap().get("id").unwrap().to_string());
-    // }
+    #[test]
+    fn with_id_property() {
+        let order_with_id = json!({"Order": {"type": "object", "properties": {"id": {"type": "integer", "format": "int64"}}}});
+        let models = convert_to_internal_model(&order_with_id).unwrap();
+        assert_eq!("Order", models.get(0).unwrap().name);
+        assert_eq!("i64".to_string(), models.get(0).unwrap().properties.as_ref().unwrap().get("id").unwrap().to_string());
+    }
 
     // #[test]
     // fn with_wrong_id_property() {
@@ -137,7 +153,7 @@ mod tests {
     //     assert_eq!("Order", models.get(0).unwrap().name);
     //     assert_eq!("OrderTwo", models.get(1).unwrap().name);
     // }
-    
+
     fn init() {
         let _ = env_logger::builder().is_test(true).filter_level(log::LevelFilter::Debug).try_init();
     }
