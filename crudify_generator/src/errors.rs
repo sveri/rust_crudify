4b6784ab3de979bbb9049748a77ec17f5622c2ee
Sven Richter <sveri80@gmail.com>
2022-10-02 17:37:55 +0200

    added oa3 example spec

---
+++ crudify_generator/src/errors.rs
@@ -0,0 +1,27 @@
+use std::{error::Error, fmt::Display};
+
+use serde_json::Value;
+
+// from https://www.reddit.com/r/rust/comments/gj8inf/comment/fqlmknt/
+#[derive(Debug)]
+pub enum JsonConverterError<'a> {
+    AsObjectError { value: &'a Value },
+}
+
+impl Error for JsonConverterError<'_> {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match *self {
+            JsonConverterError::AsObjectError { ref value } => None,
+        }
+    }
+}
+
+impl Display for JsonConverterError<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match *self {
+            Self::AsObjectError { ref value } => {
+                write!(f, "Could not convert value to object: {}", value)
+            }
+        }
+    }
+}
