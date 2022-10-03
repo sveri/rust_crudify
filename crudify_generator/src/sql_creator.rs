5a4a77803cab13073fee8c02e74e85ae924d5ce1
Sven Richter <sveri80@gmail.com>
2022-10-03 16:29:35 +0200

    got basic sql generation working

--- crudify_generator/src/sql_creator.rs
+++ crudify_generator/src/sql_creator.rs
@@ -20,16 +20,41 @@ pub fn create_create_entitiy(model: &InternalModel) -> String {
     let mut sql: String = "INSERT INTO ".to_string();
 
     let fields: String = match &model.properties {
-        None => "*".to_string(),
+        None => "".to_string(),
         Some(properties) => {
-            properties.keys().map(|p| p.as_ref()).collect::<Vec<_>>().join(", ")
+            format!("({})", properties.keys().map(|p| p.as_ref()).collect::<Vec<_>>().join(", "))
         }
     };
 
     sql.push_str(&fields);
-    sql.push_str(&format!(" FROM {}", &model.name.to_lowercase()));
+    sql.push_str(" VALUES ");
+
+
+    let values: String = match &model.properties {
+        None => "".to_string(),
+        Some(properties) => {
+            format!("({})", properties.keys().map(|_| "?").collect::<Vec<_>>().join(", "))
+        }
+    };
+
+    sql.push_str(&values);
 
     sql
+}
+
+pub fn create_update_entitiy(model: &InternalModel) -> String {
+    let fields: String = match &model.properties {
+        None => "".to_string(),
+        Some(properties) => {
+            properties.keys().map(|p| format!("{} = ?", p)).collect::<Vec<_>>().join(", ")
+        }
+    };
+
+    format!("UPDATE {} SET {} WHERE id = ?", model.name.to_lowercase(), fields)
+}
+
+pub fn create_delete_entitiy(model: &InternalModel) -> String {
+    format!("DELETE FROM {} WHERE id = ?", model.name.to_lowercase())
 }
 
 #[cfg(test)]
@@ -55,9 +80,37 @@ mod tests {
     }
 
     #[test]
+    fn test_delete_entity() {
+        let props = indexmap! {"id".to_string() => "i64".to_string(), "name".to_string() => "Sting".to_string()};
+        let expected = "DELETE FROM order WHERE id = ?";
+        assert_eq!(expected, create_delete_entitiy(&InternalModel::new_with_props("Order".to_string(), props)));
+    }
+
+    #[test]
+    fn test_update_entity_with_multiple_properties() {
+        let props = indexmap! {"id".to_string() => "i64".to_string(), "name".to_string() => "Sting".to_string()};
+        let expected = "UPDATE order SET id = ?, name = ? WHERE id = ?" ;
+        assert_eq!(expected, create_update_entitiy(&InternalModel::new_with_props("Order".to_string(), props)));
+    }
+
+    #[test]
+    fn test_update_entity_with_one_properties() {
+        let props = indexmap! {"id".to_string() => "i64".to_string()};
+        let expected = "UPDATE order SET id = ? WHERE id = ?";
+        assert_eq!(expected, create_update_entitiy(&InternalModel::new_with_props("Order".to_string(), props)));
+    }
+
+    #[test]
     fn test_create_entity_with_multiple_properties() {
         let props = indexmap! {"id".to_string() => "i64".to_string(), "name".to_string() => "Sting".to_string()};
         let expected = "INSERT INTO (id, name) VALUES (?, ?)";
+        assert_eq!(expected, create_create_entitiy(&InternalModel::new_with_props("Order".to_string(), props)));
+    }
+
+    #[test]
+    fn test_create_entity_with_one_property() {
+        let props = indexmap! {"id".to_string() => "i64".to_string()};
+        let expected = "INSERT INTO (id) VALUES (?)";
         assert_eq!(expected, create_create_entitiy(&InternalModel::new_with_props("Order".to_string(), props)));
     }
 
