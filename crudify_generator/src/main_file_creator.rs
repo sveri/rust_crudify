030512ae99b7c00edc891bb454b6c60d50af570f
Sven Richter <sveri80@gmail.com>
2022-10-11 21:46:21 +0200

    improved get entities

--- crudify_generator/src/main_file_creator.rs
+++ crudify_generator/src/main_file_creator.rs
@@ -12,16 +12,15 @@ use std::{
     fmt::{Display, Formatter},
 };
 use axum::{
-    body::Body,
     extract::{Json, Path},
-    http::{StatusCode, Request},
+    http::{StatusCode},
     response::{IntoResponse, Response},
     routing::{delete, get, post, put},
     Extension, Router,    
 };
 use serde::{Serialize, Deserialize};
 use serde_json::{json, Value};
-use sqlx::{postgres::PgPoolOptions, Pool, Postgres, PgPool, Row};
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres, PgPool, FromRow};
 use thiserror::Error;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
@@ -43,7 +42,7 @@ fn get_structs(models: &InternalModels) -> String {
             }
         };
         code.push_str(&format!(
-            "#[derive(Serialize, Deserialize)]\nstruct {} {{\n{}\n}}",
+            "#[derive(FromRow, Serialize, Deserialize)]\nstruct {} {{\n{}\n}}",
             model.name, properties
         ))
     }
@@ -56,46 +55,12 @@ fn get_routing_functions_code(models: &InternalModels) -> String {
 
     for model in models.iter() {
         code.push_str(&format!(
-            "async fn get_{}(Extension(pool): Extension<PgPool>) -> Json<Value> {{\n",
-            model.name.to_lowercase()
+            "async fn get_{}(Extension(pool): Extension<PgPool>) -> Result<Json<Value>, AppError> {{\n
+                let res: Vec<{}> = sqlx::query_as(\"{}\").fetch_all(&pool).await?;
+                Ok(Json(json!(res)))
+        }}",
+            model.name.to_lowercase(), model.name, create_get_all_entities(model)
         ));
-
-        code.push_str(&format!(
-            "let res = sqlx::query(\"{}\").fetch_all(&pool).await.unwrap();",
-            create_get_all_entities(model)
-        ));
-
-        let properties_string: String = match &model.properties {
-            None => "".to_string(),
-            Some(properties) => properties
-                .keys()
-                .map(|k| {
-                    format!(
-                        "{}: row.try_get({:?}).unwrap()",
-                        k,
-                        properties.get_index_of(k).expect("IndexMap did not return an index for key.")
-                    )
-                })
-                .collect::<Vec<_>>()
-                .join(",\n"),
-        };
-
-        let fn_code = format!(
-            r#"
-            let entities = res
-                .into_iter()
-                .map(|row| {} {{
-                    {}
-                }})
-                .collect::<Vec<{0}>>();
-            Json(json!(entities))
-        }}
-
-        "#,
-            model.name, properties_string
-        )
-        .to_string();
-        code.push_str(&fn_code);
 
         code.push_str(
             r#"
@@ -167,9 +132,9 @@ fn app(pool: Pool<Postgres>) -> Router {
     for model in models.iter() {
         let name = model.name.to_lowercase();
         code.push_str(format!(".route(\"/api/{0}\", post(post_{0}))\n", name).as_str());
-        code.push_str(format!(".route(\"/api/{0}\", put(put_{0}/:id))\n", name).as_str());
+        code.push_str(format!(".route(\"/api/{0}/:id\", put(put_{0}))\n", name).as_str());
         code.push_str(format!(".route(\"/api/{0}\", get(get_{0}))\n", name).as_str());
-        code.push_str(format!(".route(\"/api/{0}\", delete(delete_{0}/:id))\n", name).as_str());
+        code.push_str(format!(".route(\"/api/{0}/:id\", delete(delete_{0}))\n", name).as_str());
     }
     code.push_str(
         r#".merge(axum_extra::routing::SpaRouter::new("/assets", "../dist"))
